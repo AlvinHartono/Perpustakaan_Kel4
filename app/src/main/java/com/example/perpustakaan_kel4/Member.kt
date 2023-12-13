ddf97cec8b655f246cd3cd6a8282cb041ef5d233
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2023-12-13 13:26:48 +0700

    added login screen layout

---
+++ app/src/main/java/com/example/perpustakaan_kel4/Member.kt
@@ -0,0 +1,25 @@
+package com.example.perpustakaan_kel4
+
+class Member {
+    var id_member: Int = 0
+        get() =field
+        set(value) {field = value}
+
+    var nama_member : String = ""
+        get() = field
+        set(value) {field = value}
+
+    var email : String = ""
+        get() = field
+        set(value) {field = value}
+
+    var no_telp : String = ""
+        get() = field
+        set(value) {field = value}
+
+    var password : String = ""
+        get() = field
+        set(value) {field = value}
+
+
+}
--- app/src/main/java/com/example/perpustakaan_kel4/WelcomeScreen.kt
+++ app/src/main/java/com/example/perpustakaan_kel4/WelcomeScreen.kt
@@ -1,11 +1,20 @@
 package com.example.perpustakaan_kel4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
+    val buttonClick = findViewById<Button>(R.id.getStarted)
+        buttonClick.setOnClickListener{
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
