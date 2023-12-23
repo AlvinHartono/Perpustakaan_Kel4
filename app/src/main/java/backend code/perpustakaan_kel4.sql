f529744737c838ca8f076caaa389ca0a29bf46e3
yongteck0528 <aateck.2002@gmail.com>
2023-12-23 10:57:19 +0700

    updated sql added all table

--- app/src/main/java/backend code/perpustakaan_kel4.sql
+++ app/src/main/java/backend code/perpustakaan_kel4.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2023 at 07:28 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Dec 23, 2023 at 04:56 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.3.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(10) NOT NULL,
+  `judul_buku` varchar(100) NOT NULL,
+  `penerbit` varchar(100) NOT NULL,
+  `pengarang` varchar(100) NOT NULL,
+  `tahun_terbit` year(4) NOT NULL,
+  `id_kategori` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `input`
+--
+
+CREATE TABLE `input` (
+  `id_buku` int(10) NOT NULL,
+  `id_librarian` int(10) NOT NULL,
+  `tgl_input` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` varchar(10) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE `librarian` (
+  `id_librarian` int(10) NOT NULL,
+  `nama_librarian` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_telp` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `member`
 --
 
@@ -34,7 +86,7 @@ CREATE TABLE `member` (
   `email` varchar(50) DEFAULT NULL,
   `no_telp` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `member`
@@ -45,9 +97,48 @@ INSERT INTO `member` (`id_member`, `first_name_member`, `last_name_member`, `ema
 (5, 'Alvin', 'Hartono', 'alvinhartono68@gmail.com', '085691578116', '8ddcff3a80f4189ca1c9d4d902c3c909'),
 (6, '234', 'asdfasdf', 'asdfasdfs', '345345345', '8ddcff3a80f4189ca1c9d4d902c3c909');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pinjam`
+--
+
+CREATE TABLE `pinjam` (
+  `id_buku` int(10) NOT NULL,
+  `id_member` int(10) NOT NULL,
+  `tgl_peminjaman` date NOT NULL,
+  `tgl_pengembalian` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD KEY `fk_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `input`
+--
+ALTER TABLE `input`
+  ADD KEY `fk_buku` (`id_buku`),
+  ADD KEY `fk_librarian` (`id_librarian`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `librarian`
+--
+ALTER TABLE `librarian`
+  ADD PRIMARY KEY (`id_librarian`);
 
 --
 -- Indexes for table `member`
@@ -58,14 +149,51 @@ ALTER TABLE `member`
   ADD UNIQUE KEY `no_telp` (`no_telp`);
 
 --
+-- Indexes for table `pinjam`
+--
+ALTER TABLE `pinjam`
+  ADD KEY `fk_member` (`id_member`),
+  ADD KEY `fk_buku_pinjam` (`id_buku`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `librarian`
+--
+ALTER TABLE `librarian`
+  MODIFY `id_librarian` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
   MODIFY `id_member` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `fk_kategori` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `input`
+--
+ALTER TABLE `input`
+  ADD CONSTRAINT `fk_buku` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_librarian` FOREIGN KEY (`id_librarian`) REFERENCES `librarian` (`id_librarian`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pinjam`
+--
+ALTER TABLE `pinjam`
+  ADD CONSTRAINT `fk_buku_pinjam` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_member` FOREIGN KEY (`id_member`) REFERENCES `member` (`id_member`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
