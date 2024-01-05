77f4ef48577eee3f8a0fd79b21464d350c1cc43c
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2024-01-05 17:57:19 +0700

    latest commit

--- app/src/main/java/com/example/perpustakaan_kel4/BukuViewModel.kt
+++ app/src/main/java/com/example/perpustakaan_kel4/BukuViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.perpustakaan_kel4
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class BukuViewMode : ViewModel() {
 
+    val currentBookList : MutableLiveData<List<Buku>> by lazy {
+        MutableLiveData<List<Buku>>()
+    }
+
+    fun updateMemberData(updatedBookList: List<Buku>){
+        currentBookList.value = updatedBookList
+    }
 }
--- app/src/main/java/com/example/perpustakaan_kel4/RecyclerViewBookAdapter.kt
+++ app/src/main/java/com/example/perpustakaan_kel4/RecyclerViewBookAdapter.kt
@@ -1,18 +1,26 @@
 package com.example.perpustakaan_kel4
 
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
-class RecyclerViewBookAdapter(private val getActivity: FragmentActivity, private val booklist: List<Buku>) :
+class RecyclerViewBookAdapter(
+    private val getActivity: FragmentActivity,
+    private val booklist: List<Buku>
+) :
     RecyclerView.Adapter<RecyclerViewBookAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_books_list, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_books_list, parent, false)
         return MyViewHolder(view)
     }
 
@@ -20,13 +28,28 @@ class RecyclerViewBookAdapter(private val getActivity: FragmentActivity, private
         return booklist.size
     }
 
+    @OptIn(ExperimentalEncodingApi::class)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val blobString = booklist[position].image_buku
+        val imageBytes = Base64.decode(blobString)
+        val bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size);
+
+//        val bitmap = convertBlobStringToBitmap(blobString)
+
+
+        holder.bookTitle.text = booklist[position].judul_buku
+        if (bitmap != null) {
+            holder.bookimg.setImageBitmap(bitmap)
+
+        }
 
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val bookimg : ImageView = itemView.findViewById(R.id.imgBook)
-        val cardView : CardView = itemView.findViewById(R.id.cardViewList)
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val bookimg: ImageView = itemView.findViewById(R.id.imgBook)
+        val bookTitle: TextView = itemView.findViewById(R.id.BookTitle)
+        val cardView: CardView = itemView.findViewById(R.id.cardViewList)
 
     }
 }
